39a459af85cadef06a53b286dba1751ef18f7256
Ethan <38486990+ByCubed7@users.noreply.github.com>
2024-03-19 08:47:52 +0000

    Created battle scene sketch

---
+++ Scripts/BattleController.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class BattleController : Node2D
+{
+	[Export] public Camera2D Camera;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+}

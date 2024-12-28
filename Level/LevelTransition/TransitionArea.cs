6f345cbedb409af0d7a034f094fb83c85937adf5
Ethan <ByCubed7+github@gmail.com>
2024-12-28 19:43:51 +0000

    Transitions now override next spawn position

--- Level/LevelTransition/TransitionArea.cs
+++ Level/LevelTransition/TransitionArea.cs
@@ -5,6 +5,9 @@ public partial class TransitionArea : Area2D
 {
     [Export(PropertyHint.File, "*.tscn,")]
     public string targetScenePath;
+
+    [Export]
+    public Vector2I spawnPosition;
     
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -17,7 +20,10 @@ public partial class TransitionArea : Area2D
         if (body is not Player)
             return;
 
-        Tween tween = Game.Controller.screenFade.Fade(Colors.Black, 1);
+        GD.Print($"spawnPosition: {spawnPosition}");
+
+        Tween tween = Game.Controller.screenFade.Fade(Colors.Black, 1); 
+        tween.TweenCallback(Callable.From(() => { Game.Level.OverrideNextPlayerSpawnPosition(spawnPosition); }));
         tween.TweenCallback(Callable.From(() => { Game.Controller.LoadLevel(targetScenePath); }));
         Game.Controller.screenFade.FadeOut(1, tween);
     }
